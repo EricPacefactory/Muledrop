03284c8e086b088e6494b48c43310e521f7f5844
Eric <eric@pacefactory.com>
2020-07-08 15:38:26 -0400

    Added new 'contains target rgb' station + config utility

---
+++ configuration_utilities/stations/contains_target_rgb.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 11:30:12 2020
+
+@author: eo
+"""
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Add local path
+
+import os
+import sys
+
+def find_path_to_local(target_folder = "local"):
+    
+    # Skip path finding if we successfully import the dummy file
+    try:
+        from local.dummy import dummy_func; dummy_func(); return
+    except ImportError:
+        print("", "Couldn't find local directory!", "Searching for path...", sep="\n")
+    
+    # Figure out where this file is located so we can work backwards to find the target folder
+    file_directory = os.path.dirname(os.path.abspath(__file__))
+    path_check = []
+    
+    # Check parent directories to see if we hit the main project directory containing the target folder
+    prev_working_path = working_path = file_directory
+    while True:
+        
+        # If we find the target folder in the given directory, add it to the python path (if it's not already there)
+        if target_folder in os.listdir(working_path):
+            if working_path not in sys.path:
+                tilde_swarm = "~"*(4 + len(working_path))
+                print("\n{}\nPython path updated:\n  {}\n{}".format(tilde_swarm, working_path, tilde_swarm))
+                sys.path.append(working_path)
+            break
+        
+        # Stop if we hit the filesystem root directory (parent directory isn't changing)
+        prev_working_path, working_path = working_path, os.path.dirname(working_path)
+        path_check.append(prev_working_path)
+        if prev_working_path == working_path:
+            print("\nTried paths:", *path_check, "", sep="\n  ")
+            raise ImportError("Can't find '{}' directory!".format(target_folder))
+            
+find_path_to_local()
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Imports
+
+import cv2
+import numpy as np
+
+from local.lib.launcher_utils.configuration_loaders import Reconfigurable_Single_Station_Loader
+from local.lib.launcher_utils.video_processing_loops import Station_Processing_Video_Loop
+
+from local.lib.ui_utils.display_specification import Input_Display
+
+from local.configurables.stations._helper_functions import Zoomed_Station_Display
+from local.configurables.stations._helper_functions import Leveled_Data_Display, Boolean_Result_Display
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define displays
+
+class Count_Levels_Display(Leveled_Data_Display):
+    
+    # .................................................................................................................
+    
+    def __init__(self, layout_index, num_rows, num_columns, initial_display = False,
+                 lower_level_color = (150, 150, 150),
+                 upper_level_color = (125, 125, 125),
+                 display_width = 500,
+                 display_height = 256):
+        
+        # Inherit from parent class
+        super().__init__(layout_index, num_rows, num_columns, initial_display,
+                         window_name = "Count (Levels)",
+                         ch1_color = (255, 255, 255),
+                         minimum_value = 0,
+                         maximum_value = 1000,
+                         lower_level_color = lower_level_color,
+                         upper_level_color = upper_level_color,
+                         display_width = display_width,
+                         display_height = display_height)
+    
+    # .................................................................................................................
+    
+    def get_levels(self, configurable_ref):
+        return (configurable_ref.low_count, configurable_ref.high_count)
+    
+    # .................................................................................................................
+    
+    def get_latest_plot_data(self, configurable_ref):
+        return configurable_ref._latest_norm_count_int_for_config
+    
+    # .................................................................................................................
+    # .................................................................................................................
+
+
+# =====================================================================================================================
+# =====================================================================================================================
+
+
+class Station_Display(Zoomed_Station_Display):
+    
+    # .................................................................................................................
+    
+    def __init__(self, layout_index, num_rows, num_columns, initial_display = False):
+        
+        # Inherit from parent class
+        super().__init__(layout_index, num_rows, num_columns, initial_display = initial_display)
+    
+    # .................................................................................................................
+    
+    def postprocess_cropmasked_frame(self, cropmasked_frame, configurable_ref):
+        
+        # Get each color channel separately for convenience
+        red_ch_img = cropmasked_frame[:, :, 2]
+        green_ch_img = cropmasked_frame[:, :, 1]
+        blue_ch_img = cropmasked_frame[:, :, 0]
+        
+        # Get masking based on red channel settings
+        low_red = configurable_ref.low_red
+        high_red = configurable_ref.high_red
+        invert_red = configurable_ref.invert_red
+        red_ch_mask = configurable_ref._check_in_range(red_ch_img, low_red, high_red, invert_red)
+        
+        # Get masking based on green channel settings
+        low_green = configurable_ref.low_green
+        high_green = configurable_ref.high_green
+        invert_green = configurable_ref.invert_green
+        green_ch_mask = configurable_ref._check_in_range(green_ch_img, low_green, high_green, invert_green)
+        
+        # Get masking based on blue channel settings
+        low_blue = configurable_ref.low_blue
+        high_blue = configurable_ref.high_blue
+        invert_blue = configurable_ref.invert_blue
+        blue_ch_mask = configurable_ref._check_in_range(blue_ch_img, low_blue, high_blue, invert_blue)
+        
+        # Create a mask from the separate channels, which we'll apply to the proper color image for display
+        combined_mask_1ch = np.uint8(255 * np.bitwise_and(blue_ch_mask, np.bitwise_and(green_ch_mask, red_ch_mask)))
+        combined_mask_3ch = cv2.cvtColor(combined_mask_1ch, cv2.COLOR_GRAY2BGR)
+        
+        return cv2.bitwise_and(cropmasked_frame, combined_mask_3ch)
+    
+    # .................................................................................................................
+    # .................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Main
+
+# For clarity
+target_script_name = "contains_target_rgb_station"
+target_class_name = "Contains_Target_RGB_Station"
+
+# Make all required selections
+loader = Reconfigurable_Single_Station_Loader(target_script_name, target_class_name)
+loader.selections()
+loader.select_station()
+
+# Set up video capture, processing stages & playback control
+configurable_ref = loader.setup_all(__file__)
+
+# Get drawing specification for the given edge decay variable
+zone_drawing_spec = configurable_ref.get_drawing_spec("station_zones_list")
+
+# Set up object to handle all video processing
+main_process = \
+Station_Processing_Video_Loop(loader,
+                              ordered_display_list = [Boolean_Result_Display(1, 1, 4),
+                                                      Count_Levels_Display(0, 4, 1),
+                                                      Station_Display(2, 3, 3),
+                                                      Input_Display(1, 4, 1,
+                                                                    window_name = "Draw Station Zones",
+                                                                    drawing_json = zone_drawing_spec)])
+
+# Most of the work is done here!
+main_process.loop()
+
+# Ask user to save config
+loader.ask_to_save_configurable_cli(__file__, configurable_ref)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% For debugging
+
+# Access results for debugging
+final_frame = main_process.debug_frame
+final_fed_time_args = main_process.debug_fed_time_args
+debug_dict = main_process.debug_dict
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Scrap
+
---
+++ local/configurables/stations/contains_target_rgb_station.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 11:12:15 2020
+
+@author: eo
+"""
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Add local path
+
+import os
+import sys
+
+def find_path_to_local(target_folder = "local"):
+
+    # Skip path finding if we successfully import the dummy file
+    try:
+        from local.dummy import dummy_func; dummy_func(); return
+    except ImportError:
+        print("", "Couldn't find local directory!", "Searching for path...", sep="\n")
+
+    # Figure out where this file is located so we can work backwards to find the target folder
+    file_directory = os.path.dirname(os.path.abspath(__file__))
+    path_check = []
+
+    # Check parent directories to see if we hit the main project directory containing the target folder
+    prev_working_path = working_path = file_directory
+    while True:
+
+        # If we find the target folder in the given directory, add it to the python path (if it's not already there)
+        if target_folder in os.listdir(working_path):
+            if working_path not in sys.path:
+                tilde_swarm = "~"*(4 + len(working_path))
+                print("\n{}\nPython path updated:\n  {}\n{}".format(tilde_swarm, working_path, tilde_swarm))
+                sys.path.append(working_path)
+            break
+
+        # Stop if we hit the filesystem root directory (parent directory isn't changing)
+        prev_working_path, working_path = working_path, os.path.dirname(working_path)
+        path_check.append(prev_working_path)
+        if prev_working_path == working_path:
+            print("\nTried paths:", *path_check, "", sep="\n  ")
+            raise ImportError("Can't find '{}' directory!".format(target_folder))
+
+find_path_to_local()
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Imports
+
+import numpy as np
+
+from local.configurables.stations.reference_station import Reference_Station
+from local.configurables.stations._helper_functions import inmask_pixels_3ch, build_cropping_dataset
+
+from local.eolib.video.imaging import crop_pixels_in_place
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define classes
+
+
+class Contains_Target_RGB_Station(Reference_Station):
+    
+    # .................................................................................................................
+    
+    def __init__(self, station_name, cameras_folder_path, camera_select, video_wh):
+        
+        # Inherit from base class
+        super().__init__(station_name, cameras_folder_path, camera_select, video_wh, file_dunder = __file__)
+        
+        # Allocate space for derived variables
+        self._crop_y1y2x1x2 = None
+        self._cropmask_2d3ch = None
+        self._logical_cropmask_1ch = None
+        self._in_range_max_count = None
+        self._count_gamma_power = None
+        self._needs_gamma_correct = None
+        
+        # Allocate space for numpy-based range checks
+        self._low_check_array = None
+        self._high_check_array = None
+        self._invert_array = None
+        
+        # Allocate space for variables to help with visualization during re-configuring
+        self._latest_norm_count_int_for_config = None
+        
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Drawing Controls  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        self.station_zones_list = \
+        self.ctrl_spec.attach_drawing(
+                "station_zones_list",
+                default_value = [[[0.48, 0.48], [0.52, 0.48], [0.52,0.52], [0.48, 0.52]]],
+                min_max_entities = (1, 1),
+                min_max_points = (3, None),
+                entity_type = "polygon",
+                drawing_style = "zone")
+        
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Control Group 1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        self.ctrl_spec.new_control_group("Red Controls")
+        
+        self.high_red = \
+        self.ctrl_spec.attach_slider(
+                "high_red",
+                label = "High value (red)",
+                default_value = 250,
+                min_value = 0,
+                max_value = 255,
+                return_type = int,
+                tooltip = ["Only pixels whose red channel is less than (or equal) to this value will be",
+                           "'counted' as being in the drawn station"])
+        
+        self.low_red = \
+        self.ctrl_spec.attach_slider(
+                "low_red",
+                label = "Low value (red)",
+                default_value = 10,
+                min_value = 0,
+                max_value = 255,
+                return_type = int,
+                tooltip = ["Only pixels whose red channel is greater (or equal) to this value will be",
+                           "'counted' as being in the drawn station"])
+        
+        self.invert_red = \
+        self.ctrl_spec.attach_toggle(
+                "invert_red",
+                label = "Invert red target",
+                default_value = False,
+                tooltip = ["If True, the normal red channel behavior will be inverted, so that pixels",
+                           "outside the upper/lower range will be counted as being in the station"])
+        
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Control Group 2 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        self.ctrl_spec.new_control_group("Green Controls")
+        
+        self.high_green = \
+        self.ctrl_spec.attach_slider(
+                "high_green",
+                label = "High value (green)",
+                default_value = 255,
+                min_value = 0,
+                max_value = 255,
+                return_type = int,
+                tooltip = ["Only pixels whose green channel is less than (or equal) to this value will be",
+                           "'counted' as being in the drawn station"])
+        
+        self.low_green = \
+        self.ctrl_spec.attach_slider(
+                "low_green",
+                label = "Low value (green)",
+                default_value = 0,
+                min_value = 0,
+                max_value = 255,
+                return_type = int,
+                tooltip = ["Only pixels whose green channel is greater (or equal) to this value will be",
+                           "'counted' as being in the drawn station"])
+        
+        self.invert_green = \
+        self.ctrl_spec.attach_toggle(
+                "invert_green",
+                label = "Invert green target",
+                default_value = False,
+                tooltip = ["If True, the normal green channel behavior will be inverted, so that pixels",
+                           "outside the upper/lower range will be counted as being in the station"])
+        
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Control Group 3 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        self.ctrl_spec.new_control_group("Blue Controls")
+        
+        self.high_blue = \
+        self.ctrl_spec.attach_slider(
+                "high_blue",
+                label = "High value (blue)",
+                default_value = 255,
+                min_value = 0,
+                max_value = 255,
+                return_type = int,
+                tooltip = ["Only pixels whose blue channel is less than (or equal) to this value will be",
+                           "'counted' as being in the drawn station"])
+        
+        self.low_blue = \
+        self.ctrl_spec.attach_slider(
+                "low_blue",
+                label = "Low value (blue)",
+                default_value = 0,
+                min_value = 0,
+                max_value = 255,
+                return_type = int,
+                tooltip = ["Only pixels whose blue channel is greater (or equal) to this value will be",
+                           "'counted' as being in the drawn station"])
+        
+        self.invert_blue = \
+        self.ctrl_spec.attach_toggle(
+                "invert_blue",
+                label = "Invert blue target",
+                default_value = False,
+                tooltip = ["If True, the normal blue channel behavior will be inverted, so that pixels",
+                           "outside the upper/lower range will be counted as being in the station"])
+    
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Control Group 4 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        self.ctrl_spec.new_control_group("Count Controls")
+        
+        self.high_count = \
+        self.ctrl_spec.attach_slider(
+                "high_count",
+                label = "High count",
+                default_value = 1000,
+                min_value = 0,
+                max_value = 1000,
+                return_type = int,
+                units = "normalized",
+                tooltip = ["When the (normalized) count of target RGB pixels is below this value",
+                           "and above the low count threshold, the station will output a True (1) value",
+                           "otherwise it will output a False (0) value"])
+        
+        self.low_count = \
+        self.ctrl_spec.attach_slider(
+                "low_count",
+                label = "Low count",
+                default_value = 500,
+                min_value = 0,
+                max_value = 1000,
+                return_type = int,
+                units = "normalized",
+                tooltip = ["When the (normalized) count of target RGB pixels is above this value",
+                           "and below the high count threshold, the station will output a True (1) value",
+                           "otherwise it will output a False (0) value"])
+        
+        self.invert_count = \
+        self.ctrl_spec.attach_toggle(
+                "invert_count",
+                label = "Invert count target",
+                default_value = False,
+                tooltip = ["If True, the count thresholding behavior is inverted, so that the station",
+                           "will output True (1) when the count is outside the low-to-high count range",
+                           "and False (0) when inside the range"])
+        
+        self.count_correction = \
+        self.ctrl_spec.attach_slider(
+                "count_correction",
+                label = "Count scaling correction",
+                default_value = 0,
+                min_value = -50,
+                max_value = 50,
+                step_size = 1,
+                return_type = int,
+                tooltip = ["This value applies a gamma correction to the normalized count value before",
+                           "applying the low/high thresholding. This allows scaling the count values up or down",
+                           "It is intended to be used to provide better resolution on values in cases",
+                           "where the counts may be consistently near the lowest or highest",
+                           "end of the full scale"])
+    
+    # .................................................................................................................
+    
+    def close(self, final_frame_index, final_epoch_ms, final_datetime):
+        # Nothing open/in-use, so do nothing on close
+        return
+    
+    # .................................................................................................................
+    
+    def setup(self, variables_changed_dict):
+        
+        # Re-generate crop co-ordinates & logical cropmasks in case the zone was re-drawn
+        zone_crop_coords_list, cropmask_2d3ch_list, logical_cropmasks_1ch_list = \
+        build_cropping_dataset(self.video_wh, self.station_zones_list)
+        
+        # Store crop & masking data for a single zone, since the configuration forces 1 zone only
+        self._crop_y1y2x1x2 = zone_crop_coords_list[0]
+        self._cropmask_2d3ch = cropmask_2d3ch_list[0]
+        self._logical_cropmask_1ch = logical_cropmasks_1ch_list[0]
+        
+        # Store the maximum number of pixels we could get 'in-range', after accounting for masking
+        self._in_range_max_count = np.float32(np.count_nonzero(self._logical_cropmask_1ch))
+        
+        # Pre-generate comparison arrays to speed up in-range checks
+        self._low_check_array = np.int32((self.low_blue, self.low_green, self.low_red))
+        self._high_check_array = np.int32((self.high_blue, self.high_green, self.high_red))
+        self._invert_array = np.int32((self.invert_blue, self.invert_green, self.invert_red))
+        
+        # Pre-generate gamma correction factor and decide if we even need to use it!
+        self._needs_gamma_correct = (self.count_correction != 0)
+        self._count_gamma_power = np.power(1.075, self.count_correction)
+    
+    # .................................................................................................................
+    
+    def process_one_frame(self, frame, current_frame_index, current_epoch_ms, current_datetime):
+        
+        # Crop & mask the zone
+        cropped_frame = crop_pixels_in_place(frame, self._crop_y1y2x1x2)
+        cropmask_values_1d_array = inmask_pixels_3ch(cropped_frame, self._logical_cropmask_1ch)
+        
+        # For convenience
+        red_ch = cropmask_values_1d_array[:, 2]
+        green_ch = cropmask_values_1d_array[:, 1]
+        blue_ch = cropmask_values_1d_array[:, 0]
+        
+        # Figure out which pixels are in the correct rgb target range
+        in_red_range = self._check_in_range(red_ch, self.low_red, self.high_red, self.invert_red)
+        in_green_range = self._check_in_range(green_ch, self.low_green, self.high_green, self.invert_green)
+        in_blue_range = self._check_in_range(blue_ch, self.low_blue, self.high_blue, self.invert_blue)        
+        in_rgb_range = np.bitwise_and(in_red_range, in_green_range, in_blue_range)
+        
+        # Count the number of pixels in range and normalize to a 0-1000 integer value
+        num_pixels_in_range = np.float32(np.count_nonzero(in_rgb_range))
+        norm_count = (num_pixels_in_range / self._in_range_max_count)
+        if self._needs_gamma_correct:
+            norm_count = np.power(norm_count, self._count_gamma_power)
+        norm_count_int = int(round(1000 * norm_count))
+        
+        # Store integer normalized count for display purposes (only during config)
+        self._latest_norm_count_int_for_config = norm_count_int
+        
+        # Output is determined based on pixel count being within a target range
+        one_frame_result = int(self.invert_count ^ (self.low_count <= norm_count_int <= self.high_count))
+        
+        return one_frame_result
+    
+    # .................................................................................................................
+    
+    def _check_in_range(self, channel_values, low_value, high_value, invert_result):
+        
+        ''' Helper function for checking which pixels (in a given channel!) are in the target range '''
+        
+        return np.bitwise_xor(invert_result, np.bitwise_and(channel_values >= low_value, channel_values <= high_value))
+    
+    # .................................................................................................................
+    # .................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define functions
+
+# .....................................................................................................................
+
+# .....................................................................................................................
+# .....................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Demo
+
+if __name__ == "__main__":
+    pass
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Scrap
+
+
