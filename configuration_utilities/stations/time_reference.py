275a00c4e7d8d41d6ffd3a3c1b6be2375d659b28
Eric <eric@pacefactory.com>
2020-07-13 16:49:22 -0400

    Added new 'time reference' station + config util. Provides timing info (epoch ms) for every frame. Mostly intended for debugging

---
+++ configuration_utilities/stations/time_reference.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 11:03:45 2020
+
+@author: eo
+"""
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Add local path
+
+import os
+import sys
+
+def find_path_to_local(target_folder = "local"):
+    
+    # Skip path finding if we successfully import the dummy file
+    try:
+        from local.dummy import dummy_func; dummy_func(); return
+    except ImportError:
+        print("", "Couldn't find local directory!", "Searching for path...", sep="\n")
+    
+    # Figure out where this file is located so we can work backwards to find the target folder
+    file_directory = os.path.dirname(os.path.abspath(__file__))
+    path_check = []
+    
+    # Check parent directories to see if we hit the main project directory containing the target folder
+    prev_working_path = working_path = file_directory
+    while True:
+        
+        # If we find the target folder in the given directory, add it to the python path (if it's not already there)
+        if target_folder in os.listdir(working_path):
+            if working_path not in sys.path:
+                tilde_swarm = "~"*(4 + len(working_path))
+                print("\n{}\nPython path updated:\n  {}\n{}".format(tilde_swarm, working_path, tilde_swarm))
+                sys.path.append(working_path)
+            break
+        
+        # Stop if we hit the filesystem root directory (parent directory isn't changing)
+        prev_working_path, working_path = working_path, os.path.dirname(working_path)
+        path_check.append(prev_working_path)
+        if prev_working_path == working_path:
+            print("\nTried paths:", *path_check, "", sep="\n  ")
+            raise ImportError("Can't find '{}' directory!".format(target_folder))
+            
+find_path_to_local()
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Imports
+
+from local.lib.launcher_utils.configuration_loaders import Reconfigurable_Single_Station_Loader
+from local.lib.launcher_utils.video_processing_loops import Station_Processing_Video_Loop
+from local.lib.ui_utils.display_specification import Input_Display
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define displays
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Main
+
+# For clarity
+target_script_name = "timeref_station"
+target_class_name = "Time_Reference_Station"
+
+# Make all required selections
+loader = Reconfigurable_Single_Station_Loader(target_script_name, target_class_name)
+loader.selections()
+loader.select_station()
+
+# Set up video capture, processing stages & playback control
+configurable_ref = loader.setup_all(__file__)
+
+# Set up object to handle all video processing
+main_process = \
+Station_Processing_Video_Loop(loader,
+                              ordered_display_list = [Input_Display(0, 3, 3)])
+
+# Most of the work is done here!
+main_process.loop()
+
+# Ask user to save config
+loader.ask_to_save_configurable_cli(__file__, configurable_ref)
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% For debugging
+
+# Access results for debugging
+final_frame = main_process.debug_frame
+final_fed_time_args = main_process.debug_fed_time_args
+debug_dict = main_process.debug_dict
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Scrap
+
+
---
+++ local/configurables/stations/timeref_station.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 13 11:00:17 2020
+
+@author: eo
+"""
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Add local path
+
+import os
+import sys
+
+def find_path_to_local(target_folder = "local"):
+
+    # Skip path finding if we successfully import the dummy file
+    try:
+        from local.dummy import dummy_func; dummy_func(); return
+    except ImportError:
+        print("", "Couldn't find local directory!", "Searching for path...", sep="\n")
+
+    # Figure out where this file is located so we can work backwards to find the target folder
+    file_directory = os.path.dirname(os.path.abspath(__file__))
+    path_check = []
+
+    # Check parent directories to see if we hit the main project directory containing the target folder
+    prev_working_path = working_path = file_directory
+    while True:
+
+        # If we find the target folder in the given directory, add it to the python path (if it's not already there)
+        if target_folder in os.listdir(working_path):
+            if working_path not in sys.path:
+                tilde_swarm = "~"*(4 + len(working_path))
+                print("\n{}\nPython path updated:\n  {}\n{}".format(tilde_swarm, working_path, tilde_swarm))
+                sys.path.append(working_path)
+            break
+
+        # Stop if we hit the filesystem root directory (parent directory isn't changing)
+        prev_working_path, working_path = working_path, os.path.dirname(working_path)
+        path_check.append(prev_working_path)
+        if prev_working_path == working_path:
+            print("\nTried paths:", *path_check, "", sep="\n  ")
+            raise ImportError("Can't find '{}' directory!".format(target_folder))
+
+find_path_to_local()
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Imports
+
+from local.configurables.stations.reference_station import Reference_Station
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define classes
+
+
+class Time_Reference_Station(Reference_Station):
+    
+    # .................................................................................................................
+    
+    def __init__(self, station_name, cameras_folder_path, camera_select, video_wh):
+        
+        # Inherit from base class
+        super().__init__(station_name, cameras_folder_path, camera_select, video_wh, file_dunder = __file__)
+        
+        # Allocate space for derived variables
+        self._crop_y1y2x1x2 = None
+        self._cropmask_2d3ch = None
+        self._logical_cropmask_1ch = None
+        
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Drawing Controls  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        self.station_zones_list = \
+        self.ctrl_spec.attach_drawing(
+                "station_zones_list",
+                default_value = [[[0.0, 0.0], [1.0, 0.0], [1.0, 1.0], [0.0, 1.0]]],
+                min_max_entities = (1, 1),
+                min_max_points = (3, None),
+                entity_type = "polygon",
+                drawing_style = "zone")
+        
+        # .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  . Control Group 1 .  .  .  .  .  .  .  .  .  .  .  .  .  .  .  .
+        
+        #self.ctrl_spec.new_control_group("General Controls")
+    
+    # .................................................................................................................
+    
+    def close(self, final_frame_index, final_epoch_ms, final_datetime):
+        # Nothing open/in-use, so do nothing on close
+        return
+    
+    # .................................................................................................................
+    
+    def setup(self, variables_changed_dict):
+        
+        # No controls, so nothing to setup
+        # (Zone/Drawing isn't actually used for anything!)
+        
+        return
+    
+    # .................................................................................................................
+    
+    def process_one_frame(self, frame, current_frame_index, current_epoch_ms, current_datetime):
+        
+        # Store timing info for every frame
+        one_frame_result = current_epoch_ms
+        
+        return one_frame_result
+    
+    # .................................................................................................................
+    # .................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Define functions
+
+# .....................................................................................................................
+
+# .....................................................................................................................
+# .....................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Demo
+
+if __name__ == "__main__":
+    pass
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Scrap
+
+
