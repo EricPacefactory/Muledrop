b4c1763dbe4433e9e0b5313628677e5f57b7ac9a
Eric <eric@pacefactory.com>
2020-07-20 15:23:31 -0400

    Updated eolib cli tools library

--- local/eolib/utils/cli_tools.py
+++ local/eolib/utils/cli_tools.py
@@ -584,8 +584,9 @@ class Datetime_Input_Parser:
     # .................................................................................................................
     # .................................................................................................................
 
+
 # ---------------------------------------------------------------------------------------------------------------------
-#%% Define Functions
+#%% Environment/control functions
 
 # .....................................................................................................................
 
@@ -599,6 +600,13 @@ def _safe_quit():
     if _using_spyder():
         raise SystemExit("Spyder-friendly quit")
     quit()
+
+# .....................................................................................................................
+# .....................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% Decorators
 
 # .....................................................................................................................
 
@@ -688,6 +696,13 @@ def loop_on_value_error(function_to_loop):
         return return_value
     
     return looping_function
+
+# .....................................................................................................................
+# .....................................................................................................................
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+#%% CLI Input functions
 
 # .....................................................................................................................
 
@@ -1139,7 +1154,63 @@ def cli_confirm(confirm_text,
 
 
 # ---------------------------------------------------------------------------------------------------------------------
-#%% Datetime parsing functions
+#%% Helper functions
+
+# .....................................................................................................................
+
+def reorder_list(entry_list, ordered_hints_list, add_nonmatch_to_end = True):
+    
+    '''
+    Function which allows for re-ordering of a list of string based on a list of 'hints'
+    Non-matched entries can be added to the beginning or end of the ordered component of the list
+    Note that ordering by hints uses a 'starts with' check. So hints are intended to be the first few
+    (unique) characters of the desired entry order
+    
+    Inputs:
+        entry_list --> (List) List of entries to be re-ordered. Assumed to be a list of strings
+        
+        ordered_hints_list --> (List) List of 'hints' used to re-order the list of entries. Ordering works
+                             using a 'starts with' check. So the first few (unique) characters of each
+                             target entry should be provided as hints
+        
+        add_nonmatch_to_end --> (Boolean) If true, entries that are not matched to hints will be added
+                                to the end of the output list. Otherwise they will be at the start
+    
+    Outputs:
+        reordered_list
+    
+    Example:
+        reorder_list(["Japan", "Moncton", "Montreal", "Australia", "Ottawa", "Brazil", "Toronto", "Vancouver"],
+                     ["tor", "van", "monc", "ot", "mont"],
+                     True)
+        => ["Toronto", "Vancouver", "Moncton", "Ottawa", "Montreal", "Japan", "Australia", "Brazil"]
+    '''
+    
+    # Try to enfore an ordering of the input list entries
+    reordered_list = []
+    for each_hint in ordered_hints_list:
+        lowered_hint = str(each_hint).lower()
+        for each_entry in entry_list:
+            lowered_entry = str(each_entry).lower()
+            matches_hint = (lowered_entry.startswith(lowered_hint))
+            if matches_hint:
+                reordered_list.append(each_entry)
+                break
+    
+    # Add any entries that didn't match the target ordering to the listing
+    nonmatch_list = []
+    for each_entry in entry_list:
+        if each_entry not in reordered_list:
+            nonmatch_list.append(each_entry)
+    
+    # Figure out ordering of the final output, which contains matches/unmatch entries
+    output_list = (reordered_list + nonmatch_list) if add_nonmatch_to_end else (nonmatch_list + reordered_list)
+    
+    return output_list
+
+# .....................................................................................................................
+# .....................................................................................................................
+
 
 # ---------------------------------------------------------------------------------------------------------------------
 #%% Demo
